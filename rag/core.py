6aa4f1dbc7a18a5452da03d0638c50381111cb89
bangoc123 <bangoc.gdg@gmail.com>
2024-06-22 11:26:18 +0700

    Refactor rag code

---
+++ rag/core.py
@@ -0,0 +1,113 @@
+import pymongo
+import google.generativeai as genai
+from sentence_transformers import SentenceTransformer
+from IPython.display import Markdown
+import textwrap
+
+class RAG():
+    def __init__(self, 
+            mongodbUri: str,
+            dbName: str,
+            dbCollection: str,
+            llmApiKey: str,
+            llmName: str ='gemini-1.5-pro',
+            embeddingName: str ='keepitreal/vietnamese-sbert',
+        ):
+        self.client = pymongo.MongoClient(mongodbUri)
+        self.db = self.client[dbName] 
+        self.collection = self.db[dbCollection]
+        self.embedding_model = SentenceTransformer(embeddingName)
+
+        # config llm
+        genai.configure(api_key=llmApiKey)
+        self.llm = genai.GenerativeModel(llmName)
+
+    def get_embedding(self, text):
+        if not text.strip():
+            return []
+
+        embedding = self.embedding_model.encode(text)
+        return embedding.tolist()
+
+    def vector_search(
+            self, 
+            user_query: str, 
+            limit=4):
+        """
+        Perform a vector search in the MongoDB collection based on the user query.
+
+        Args:
+        user_query (str): The user's query string.
+
+        Returns:
+        list: A list of matching documents.
+        """
+
+        # Generate embedding for the user query
+        query_embedding = self.get_embedding(user_query)
+
+        if query_embedding is None:
+            return "Invalid query or embedding generation failed."
+
+        # Define the vector search pipeline
+        vector_search_stage = {
+            "$vectorSearch": {
+                "index": "vector_index",
+                "queryVector": query_embedding,
+                "path": "embedding",
+                "numCandidates": 400,
+                "limit": limit,
+            }
+        }
+
+        unset_stage = {
+            "$unset": "embedding" 
+        }
+
+        project_stage = {
+            "$project": {
+                "_id": 0,  
+                "title": 1, 
+                # "product_specs": 1,
+                "color_options": 1,
+                "current_price": 1,
+                "product_promotion": 1,
+                "score": {
+                    "$meta": "vectorSearchScore"
+                }
+            }
+        }
+
+        pipeline = [vector_search_stage, unset_stage, project_stage]
+
+        # Execute the search
+        results = self.collection.aggregate(pipeline)
+
+        return list(results)
+
+    def enhance_prompt(self, query):
+        get_knowledge = self.vector_search(query, 10)
+        enhanced_prompt = ""
+        i = 0
+        for result in get_knowledge:
+            if result.get('current_price'):
+                i += 1
+                enhanced_prompt += f"\n {i}) Tên: {result.get('title')}"
+                
+                if result.get('current_price'):
+                    enhanced_prompt += f", Giá: {result.get('current_price')}"
+                else:
+                    # Mock up data
+                    # Retrieval model pricing from the internet.
+                    enhanced_prompt += f", Giá: Liên hệ để trao đổi thêm!"
+                
+                if result.get('product_promotion'):
+                    enhanced_prompt += f", Ưu đãi: {result.get('product_promotion')}"
+        return enhanced_prompt
+
+    def generate_content(self, prompt):
+        return self.llm.generate_content(prompt)
+
+    def _to_markdown(text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
--- serve.py
+++ serve.py
@@ -1,124 +1,30 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
-import pymongo
 import google.generativeai as genai
 from flask_cors import CORS
-import pathlib
-import textwrap
-from IPython.display import display
-from IPython.display import Markdown
+from rag.core import RAG
 
 # Load environment variables from .env file
 load_dotenv()
-
 # Access the key
 MONGODB_URI = os.getenv('MONGODB_URI')
-EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL') or 'keepitreal/vietnamese-sbert'
 DB_NAME = os.getenv('DB_NAME')
 DB_COLLECTION = os.getenv('DB_COLLECTION')
-GEMINI_KEY = os.getenv('GEMINI_KEY')
-genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-pro')
-
-client = pymongo.MongoClient(MONGODB_URI)
-db = client[DB_NAME] 
-collection = db[DB_COLLECTION]
-
+LLM_KEY = os.getenv('GEMINI_KEY')
+EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL') or 'keepitreal/vietnamese-sbert'
 app = Flask(__name__)
 CORS(app)
 
-from sentence_transformers import SentenceTransformer
-embedding_model = SentenceTransformer(EMBEDDING_MODEL)
-
-def vector_search(user_query, collection, limit=4):
-    """
-    Perform a vector search in the MongoDB collection based on the user query.
-
-    Args:
-    user_query (str): The user's query string.
-    collection (MongoCollection): The MongoDB collection to search.
-
-    Returns:
-    list: A list of matching documents.
-    """
-
-    # Generate embedding for the user query
-    query_embedding = get_embedding(user_query)
-
-    if query_embedding is None:
-        return "Invalid query or embedding generation failed."
-
-    # Define the vector search pipeline
-    vector_search_stage = {
-        "$vectorSearch": {
-            "index": "vector_index",
-            "queryVector": query_embedding,
-            "path": "embedding",
-            "numCandidates": 400,
-            "limit": limit,
-        }
-    }
-
-    unset_stage = {
-        "$unset": "embedding" 
-    }
-
-    project_stage = {
-        "$project": {
-            "_id": 0,  
-            "title": 1, 
-            # "product_specs": 1,
-            "color_options": 1,
-            "current_price": 1,
-            "product_promotion": 1,
-            "score": {
-                "$meta": "vectorSearchScore"
-            }
-        }
-    }
-
-    pipeline = [vector_search_stage, unset_stage, project_stage]
-
-    # Execute the search
-    results = collection.aggregate(pipeline)
-
-    return list(results)
-
-def get_search_result(query, collection):
-    get_knowledge = vector_search(query, collection, 10)
-    search_result = ""
-    i = 0
-    for result in get_knowledge:
-        print(result)
-        if result.get('current_price'):
-            i += 1
-            search_result += f"\n {i}) Tên: {result.get('title')}"
-            
-            if result.get('current_price'):
-                search_result += f", Giá: {result.get('current_price')}"
-            else:
-                # Mock up data
-                # Retrieval model pricing from the internet.
-                search_result += f", Giá: Liên hệ để trao đổi thêm!"
-            
-            if result.get('product_promotion'):
-                search_result += f", Ưu đãi: {result.get('product_promotion')}"
-    return search_result
-
-def get_embedding(text):
-    if not text.strip():
-        print("Attempted to get embedding for empty text.")
-        return []
-
-    embedding = embedding_model.encode(text)
-
-    return embedding.tolist()
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
+# Initialize RAG
+rag = RAG(
+    mongodbUri=MONGODB_URI,
+    dbName=DB_NAME,
+    dbCollection=DB_COLLECTION,
+    llmName='gemini-1.5-pro',
+    embeddingName='keepitreal/vietnamese-sbert',
+    llmApiKey=LLM_KEY,
+)
 
 def process_query(query):
     return query.lower()
@@ -132,11 +38,10 @@ def handle_query():
         return jsonify({'error': 'No query provided'}), 400
 
     # Retrieve data from vector database
-
-    source_information = get_search_result(query, collection).replace('<br>', '\n')
+    source_information = rag.enhance_prompt(query).replace('<br>', '\n')
     combined_information = f"Hãy trở thành chuyên gia tư vấn bán hàng cho một cửa hàng điện thoại. Câu hỏi của khách hàng: {query}\nTrả lời câu hỏi dựa vào các thông tin sản phẩm dưới đây: {source_information}."
 
-    response = model.generate_content(combined_information)
+    response = rag.generate_content(combined_information)
     
     return jsonify({
         'content': response.text,
@@ -144,4 +49,4 @@ def handle_query():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
