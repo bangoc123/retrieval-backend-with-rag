5dfaeb4c6d08003bdf853996528be9ef0919d1d4
daothihuyen64 <daothihuyennv64@gmail.com>
2025-07-26 17:59:47 +0700

    add llms

---
+++ llms/llms.py
@@ -0,0 +1,14 @@
+class LLMs:
+    def __init__(self, type : str, model_name : str, engine : str = None, api_key : str = None, model_version: str = None, **kwargs):
+        """Initialize object LocalLLMs or OnlineLLMs with the provided configuration.
+            not return 
+        """
+
+    def generate_content(self, prompt: str) -> str:
+        """
+        Generate content using the LLM based on the provided prompt.
+        input: prompt (str): The prompt to generate content for.
+        output: str: The generated content.
+        """
+
+       
