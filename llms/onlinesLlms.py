e2869ccc29225e5e3aac024cc24b9351ef2e11cc
daothihuyen64 <daothihuyennv64@gmail.com>
2025-07-28 19:06:44 +0700

    add online llm

--- llms/onlinesLlms.py
+++ llms/onlinesLlms.py
@@ -1,10 +1,42 @@
+import google.generativeai as genai
+import openai
+
 class OnLineLLMs:
     def __init__(self, model_name: str, api_key: str, model_version: str):
         """Initialize model with the specified name, API key, and model version."""
+        self.model_name = model_name.lower()
+        self.model_version = model_version
+
+        if self.model_name == "gemini" and api_key:
+            genai.configure(api_key=api_key)
+            self.model = genai.GenerativeModel(model_name=model_version)
+        elif self.model_name == "openai" and api_key:
+            self.client = openai.OpenAI(api_key=api_key)
+        else:
+            raise ValueError("Unsupported model name or missing API key.")
+
 
     def generate_content(self, prompt: str) -> str:
         """Generate content using the online LLM based on the provided prompt.
             input: prompt (str): The prompt to generate content for.
             output: str: The generated content.
         """
+        if self.model_name == "gemini":
+            gemini_messages = [
+                {"role": msg["role"], "parts": [msg["content"]]} for msg in prompt
+            ]
+            response = self.model.generate_content(gemini_messages)
+            try:
+                return response.text 
+            except:
+                return response.candidates[0].content.parts[0].text
 
+        elif self.model_name == "openai":
+            response = self.client.chat.completions.create(
+                model=self.model_version,
+                messages=prompt
+            )
+            return response.choices[0].message.content
+
+        else:
+            raise ValueError(f"Unsupported model name: {self.name}")
--- serve.py
+++ serve.py
@@ -21,7 +21,7 @@ DB_NAME = os.getenv('DB_NAME')
 DB_COLLECTION = os.getenv('DB_COLLECTION')
 LLM_KEY = os.getenv('GEMINI_KEY')
 EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL') or 'keepitreal/vietnamese-sbert'
-OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
+OPEN_AI_KEY = os.getenv('OPENAI_API_KEY')
 OPEN_AI_EMBEDDING_MODEL = os.getenv('OPEN_AI_EMBEDDING_MODEL') or 'text-embedding-3-small'
 QDRANT_API = os.getenv('QDRANT_API')
 QDRANT_URL = os.getenv('QDRANT_URL')
@@ -30,8 +30,16 @@ OpenAIEmbedding(OPEN_AI_KEY)
 
 MODEL_TYPE = os.getenv("MODEL_TYPE", "offline")
 MODEL_NAME = os.getenv("MODEL_NAME", "NousResearch/Meta-Llama-3-8B-Instruct")
-MODEL_ENGINE = os.getenv("MODEL_ENGINE", "vllm")
+MODEL_ENGINE = os.getenv("MODEL_ENGINE", None)
 MODEL_BASE_URL = os.getenv("MODEL_BASE_URL", None)
+MODEL_VERSION = os.getenv("MODEL_VERSION", None)
+if MODEL_TYPE == "online" and MODEL_NAME == "gemini":
+    MODEL_API_KEY = LLM_KEY
+elif MODEL_TYPE == "online" and MODEL_NAME == "openai":
+    print("Using OpenAI API Key")
+    MODEL_API_KEY = OPEN_AI_KEY
+else:
+    MODEL_API_KEY = None
 
 # --- embedding setup --- # 
 
@@ -53,7 +61,7 @@ semanticRouter = SemanticRouter(openAIEmbeding, routes=[productRoute, chitchatRo
 
 genai.configure(api_key=LLM_KEY)
 # llm = genai.GenerativeModel('gemini-1.5-pro')
-llm = LLMs(type=MODEL_TYPE, model_name=MODEL_NAME, engine=MODEL_ENGINE, base_url=MODEL_BASE_URL)
+llm = LLMs(type=MODEL_TYPE, model_name=MODEL_NAME, engine=MODEL_ENGINE, base_url=MODEL_BASE_URL, api_key=MODEL_API_KEY, model_version=MODEL_VERSION)
 
 
 # --- End Set up LLMs --- #
